cf70c9ba71dba1885ab8e36a7086b78a80f745cd
vasamDev <narasimhuluvasam2@gmail.com>
2026-07-17 17:05:14 +0530

    added expenses tracker initial commit

--- database.py
+++ database.py
@@ -1,6 +1,23 @@
-from pymongo import MongoClient
+# from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")
+# client = MongoClient("mongodb://localhost:27017")
+# db = client["expensesTracker"]
+# users_collections =db["users"]
+# expenses_collections = db["expenses"]
+
+
+
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+
 db = client["expensesTracker"]
-users_collections =db["users"]
+
+users_collections = db["users"]
 expenses_collections = db["expenses"]
